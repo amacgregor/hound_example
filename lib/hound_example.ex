5f1e8a380ecee33803c499b73886682fdc7e3b80
Allan MacGregor <amacgregor@allanmacgregor.com>
2022-05-23 12:44:28 -0300

    Add working code for taking screenshots

--- lib/hound_example.ex
+++ lib/hound_example.ex
@@ -2,17 +2,25 @@ defmodule HoundExample do
   @moduledoc """
   Documentation for `HoundExample`.
   """
+  require Logger
+  use Hound.Helpers
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HoundExample.hello()
-      :world
-
+  @moduledoc """
+  Take a screenshot of the given URL and save it to the given filename.
   """
-  def hello do
-    :world
+  def take_screenshot(url, filename) do
+    Logger.info "Taking screenshot of #{url} and saving to #{filename}"
+
+    Hound.start_session
+
+    navigate_to url
+
+    take_screenshot("./#{filename}")
+
+    Hound.end_session
+
+    Logger.info "Screenshot saved to #{filename}"
+
+    {:ok, filename}
   end
 end
